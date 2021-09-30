71d37a2c1e73cd093efe5291c10843c87dd0f2d6
ChinhLe <ngocchinhlqd@gmail.com>
2021-09-30 09:50:50 +0700

    Init project

---
+++ src/main/java/vn/alpaca/alpacajavatraininglast2021/AlpacaJavaTrainingLast2021Application.java
@@ -0,0 +1,13 @@
+package vn.alpaca.alpacajavatraininglast2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlpacaJavaTrainingLast2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlpacaJavaTrainingLast2021Application.class, args);
+    }
+
+}
---
+++ src/test/java/vn/alpaca/alpacajavatraininglast2021/AlpacaJavaTrainingLast2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.alpaca.alpacajavatraininglast2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlpacaJavaTrainingLast2021ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
