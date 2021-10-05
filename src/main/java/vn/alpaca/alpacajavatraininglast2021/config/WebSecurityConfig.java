772913ba9d40148655d9b6260da8f664c7c10184
ChinhLe <ngocchinhlqd@gmail.com>
2021-10-05 13:46:36 +0700

    Config sessionManagement of Spring security

--- src/main/java/vn/alpaca/alpacajavatraininglast2021/config/WebSecurityConfig.java
+++ src/main/java/vn/alpaca/alpacajavatraininglast2021/config/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -50,12 +51,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .cors()
-                .and()
+                    .and()
                 .csrf()
-                .disable()
+                    .disable()
                 .authorizeRequests()
-                .antMatchers("/api/login").permitAll()
-                .anyRequest().authenticated();
+                    .antMatchers("/api/login")
+                        .permitAll()
+                    .anyRequest()
+                        .authenticated();
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.addFilterBefore(jwtAuthenticationFilter(),
                 UsernamePasswordAuthenticationFilter.class);
