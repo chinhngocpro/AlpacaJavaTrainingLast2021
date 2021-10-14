b84a5081e65c3717bd65db121efe7b4f679126df
karasuno07 <kennyjoe1311@gmail.com>
2021-10-14 18:58:16 +0700

    modify feign path

--- contract-service/src/main/java/vn/alpaca/contractservice/client/CustomerFeignClient.java
+++ contract-service/src/main/java/vn/alpaca/contractservice/client/CustomerFeignClient.java
@@ -1,16 +1,29 @@
 package vn.alpaca.contractservice.client;
 
+import feign.Feign;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import vn.alpaca.contractservice.object.wrapper.response.CustomerResponse;
 import vn.alpaca.response.wrapper.SuccessResponse;
 
-@FeignClient(value = "customer-service", path = "/customers")
+@FeignClient(value = "customer-service")
 public interface CustomerFeignClient {
 
     @GetMapping("/search/id-card/{idCardNumber}")
     SuccessResponse<CustomerResponse>
     getByIdCardNumber(@PathVariable String idCardNumber);
 
+}
+
+@LoadBalancerClient(value = "customer-service")
+class CustomerServiceLoadBalanceConfig {
+    @LoadBalanced
+    @Bean
+    public Feign.Builder feignBuilder() {
+        return Feign.builder();
+    }
 }
