daa324339075ac955724e1316583ca82b96d6240
chinh <ngocchinhlqd@gmail.com>
2021-10-14 13:27:25 +0700

    add FeignCodecConfig

--- gateway/src/main/java/vn/alpaca/gateway/config/FeignCodecConfig.java
+++ gateway/src/main/java/vn/alpaca/gateway/config/FeignCodecConfig.java
@@ -1,15 +1,27 @@
 package vn.alpaca.gateway.config;
 
 import feign.codec.Decoder;
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.support.SpringDecoder;
+import org.springframework.cloud.openfeign.support.SpringEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
-public class FeignResponseDecoderConfig {
+public class FeignCodecConfig {
+    @Bean
+    public Encoder feignFormEncoder() {
+        ObjectFactory<HttpMessageConverters> messageConverters = () -> {
+            HttpMessageConverters converters = new HttpMessageConverters();
+            return converters;
+        };
+        return new SpringFormEncoder(new SpringEncoder(messageConverters));
+    }
+
     @Bean
     public Decoder feignDecoder() {
 
